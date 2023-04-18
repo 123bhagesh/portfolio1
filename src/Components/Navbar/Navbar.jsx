752c7c9d28e1f8f3c03a4b5ccb08c04acac609ab
123bhagesh <123bhagesh@gmail.com>
2023-04-18 18:00:01 +0530

    navbar rusume download button added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,63 +6,81 @@ import { BiBook } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageDetail } from "react-icons/bi";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import resumeFile from "../../assets/BhageshResume.pdf";
 
 const Navbar = () => {
   const [activeNav, setActiveNav] = useState("#");
+
+  const handleResume = () => {
+    let alink = document.createElement("a");
+    alink.href = resumeFile;
+    alink.download = "Bhagesh Madavi Resume.pdf";
+    alink.click();
+    viewResume();
+  };
+
+  const viewResume = () => {
+    window.open(
+      "https://drive.google.com/file/d/1vcE3ji5Olc3bFHvEfoDvBWtn-SoDV9zV/view?usp=sharing"
+    );
+  };
   return (
     <div className="navbarContainer">
-      <nav>
-        <a
-          href="#"
-          onClick={() => setActiveNav("#")}
-          className={activeNav === "#" ? "active" : ""}
-        >
-          Home
-        </a>
-        <a
-          href="#about"
-          onClick={() => setActiveNav("#about")}
-          className={activeNav === "#about" ? "active" : ""}
-        >
-          About
-        </a>
-        <a
-          href="#skills"
-          onClick={() => setActiveNav("#skills")}
-          className={activeNav === "#skills" ? "active" : ""}
-        >
-          Skills
-        </a>
-        <a
-          href="#projects"
-          onClick={() => setActiveNav("#projects")}
-          className={activeNav === "#projects" ? "active" : ""}
-        >
-          Projects
-        </a>
-        <a
-          href="#github"
-          onClick={() => setActiveNav("#github")}
-          className={activeNav === "#github" ? "active" : ""}
-        >
-          Github
-        </a>
+        <nav>
+          <a
+            href="#"
+            onClick={() => setActiveNav("#")}
+            className={activeNav === "#" ? "active" : ""}
+          >
+            Home
+          </a>
+          <a
+            href="#about"
+            onClick={() => setActiveNav("#about")}
+            className={activeNav === "#about" ? "active" : ""}
+          >
+            About
+          </a>
+          <a
+            href="#skills"
+            onClick={() => setActiveNav("#skills")}
+            className={activeNav === "#skills" ? "active" : ""}
+          >
+            Skills
+          </a>
+          <a
+            href="#projects"
+            onClick={() => setActiveNav("#projects")}
+            className={activeNav === "#projects" ? "active" : ""}
+          >
+            Projects
+          </a>
+          <a
+            href="#github"
+            onClick={() => setActiveNav("#github")}
+            className={activeNav === "#github" ? "active" : ""}
+          >
+            Github
+          </a>
 
-        <a
-          href="#contact"
-          onClick={() => setActiveNav("#contact")}
-          className={activeNav === "#contact" ? "active" : ""}
-        >
-          Contact
-        </a>
-        <a
-          href="#contact"
-          onClick={() => setActiveNav("#contact")}
-          className={activeNav === "#contact" ? "active" : ""}
+          <a
+            href="#contact"
+            onClick={() => setActiveNav("#contact")}
+            className={activeNav === "#contact" ? "active" : ""}
+          >
+            Contact
+          </a>
+          <a
+          onClick={handleResume}
+          // href="https://drive.google.com/file/d/1vcE3ji5Olc3bFHvEfoDvBWtn-SoDV9zV/view?usp=sharing"
+          target="_blank"
+          className="btnResume"
         >
           Download Resume
         </a>
-      </nav>
+        </nav>
+
+
     </div>
   );
 };
