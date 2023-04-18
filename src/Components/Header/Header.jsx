6a76020048905ef4aadbf93e541763ccb385521f
123bhagesh <123bhagesh@gmail.com>
2023-04-18 14:59:36 +0530

    header component css updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,15 +3,16 @@ import "./header.css";
 // import CTA from "./CTA";
 import ME from "../../assets/Bhagesh_Photo.jpg";
 import Navbar from "../Navbar/Navbar";
-import resumeFile from '../../assets/BhageshResume.pdf'
+import resumeFile from "../../assets/BhageshResume.pdf";
 // import HeaderSocial from "./HeaderSocial";
+import Typewriter from "typewriter-effect";
 
 const handleResume = () => {
   let alink = document.createElement("a");
   alink.href = resumeFile;
   alink.download = "Bhagesh Madavi Resume.pdf";
   alink.click();
-  viewResume()
+  viewResume();
 };
 // let resumeLink=  "https://drive.google.com/file/d/1vcE3ji5Olc3bFHvEfoDvBWtn-SoDV9zV/view?usp=sharing";
 
@@ -36,22 +37,32 @@ const Header = () => {
     <div id="#" className="header">
       <header>
         <div className="header_container">
-          <center>
+          <div style={{display:'flex',flexDirection:'column',justifyContent:"center",textAlign:'center'}}>
             <h3>Hello I'm</h3>
             <h1>Bhagesh Madavi</h1>
-            <img
+            <div className="typeAnimation">
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+              delay: 40,
+              strings: [
+                "Software Developer...",
+                "Frontend Web Developer...",
+              ],
+            }}
+          />
+        </div>
+            {/* <img
               className="developerName"
               src="https://readme-typing-svg.herokuapp.com?&font=IBM+Plex+Sans&color=abcdei&size=25&lines=Frontend+Web+Developer"
-            />
-            {/* </div> */}
-            {/* <h4 className="text-light">Full-Stack Web Developer</h4> */}
+            /> */}
+
             <div className="me">
               <img src={ME} alt="Me" />
             </div>
-          </center>
-          {/* <CTA /> */}
+          </div>
           <div className="top-portion">
-            <div></div>
             <div className="about">
               <p>I am a Frontend Web Developer with a hands-on Javascript.</p>
               <p>
@@ -68,22 +79,14 @@ const Header = () => {
               >
                 Download Resume
               </a>
-              {/* <button onClick={handleResume} style={{ padding: "10px" }}>
-                DOWNLOAD Resume
-              </button> */}
+
               <a href="#contact" className="btn btn-primary">
                 Let's Talk
               </a>
             </div>
           </div>
-          {/* <HeaderSocial /> */}
-
-          {/* <a href="#contact" className="scroll__down">Scroll Down</a> */}
         </div>
       </header>
-      {/* <div className="navbar-external">
-        <Navbar />
-      </div> */}
     </div>
   );
 };
